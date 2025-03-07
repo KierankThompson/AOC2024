3ed462dc568f6bd4b27094cb4347341d31d35148
KierankThompson <thompson.kierank@gmail.com>
2025-03-06 23:06:20 -0500

    did some stuff

--- problem3.py
+++ problem3.py
@@ -3,12 +3,27 @@ import re
 ans = 0
 with open("problem3.txt") as f:
     lines = f.readlines()
-    p = re.compile("mul\(\d{1,3},\d{1,3}\)")
-    p2 = re.compile("\d{1,3}")
+    p = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)")
     for line in lines:
         for match in p.findall(line):
-            numbers = p2.findall(match)
-            ans += int(numbers[0]) * int(numbers[1])
+            ans += int(match[0]) * int(match[1])
 print(ans)
+ans = 0
+with open("problem3.txt") as f:
+    lines = f.readlines()
+   
+    enabled = True
+    for line in lines:
+        for match in re.finditer(r"do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)",line):
+            if match[0] == "don't()":
+                enabled = False
+            elif match[0] == "do()":
+                enabled = True
+            else:
+                if enabled:   
+                    ans += int(match[1]) * int(match[2])
+print(ans)
+
+
 
         
